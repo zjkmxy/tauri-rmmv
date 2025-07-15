a7c22f784d86e8a1c91c1d31180aae22a5b8cf96
Xinyu Ma <kinu@meta.com>
2025-07-14 21:29:46 -0700

    Fix in Windows

---
+++ src/rpg/next/Scene_Async.ts
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// Scene_Async
+//
+// The scene class for asynchronous operations.
+
+import { Scene_Base } from '../scenes/Scene_Base';
+
+export class Scene_Async extends Scene_Base {
+  constructor() {
+    super();
+  }
+
+  protected async main() {}
+}
